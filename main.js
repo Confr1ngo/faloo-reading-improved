6835608adbe86824669707252d388a7ca6f65212
Gong Hengjie <2682786816@qq.com>
2023-09-08 07:57:03 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 // ==UserScript==
-// @name            飞卢小说图片去除
-// @name:en         Faloo Picture Remover
-// @description     用于删除飞卢小说网页端阅读页面的图片
-// @description:en  Used to delete images from the webpage reading page of faloo novel
-// @version         0.0.2
+// @name            飞卢小说阅读页面净化
+// @description     用于删除飞卢小说网页端阅读页面的无关元素
+// @version         0.1
 // @author          Confringo
-// @namespace       https://github.com/gonghengjie/faloo-picture-remover/
+// @namespace       https://github.com/gonghengjie/faloo-reading-improved/
 // @match           *://b.faloo.com/*_*.html
 // @match           *://wap.faloo.com/*_*.html
 // @license         AGPL-3.0-or-later
@@ -14,19 +12,60 @@
 (function() {
     'use strict';
     try{
-        let e = document.evaluate("/html/body/section/div[1]/div[3]/div[1]/a/img", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-        e.singleNodeValue.remove();
+        let wapEle = document.evaluate("/html/body/section/div[1]/div[3]/div[1]/a/img", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        wapEle.singleNodeValue.remove();
+
     }
-    catch(k){
-        console.log("Script for wap.faloo.com ran into an error. Text will show below:");
-        console.log(k.message);
-    }
+    catch(k){}
     try{
-        let f = document.evaluate("/html/body/div[6]/div/div[1]/div[6]/a/img", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-        f.singleNodeValue.remove();
+        let bEle = document.evaluate("/html/body/div[6]/div/div[1]/div[6]/a/img", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
+        bEle = document.evaluate("/html/body/div[3]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
+        bEle = document.evaluate("/html/body/div[5]/div/div[1]/div[2]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
+        bEle = document.evaluate("/html/body/div[5]/div/div[1]/div[3]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
+        bEle = document.evaluate("/html/body/div[4]/div", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
+        bEle = document.evaluate("/html/body/div[5]/div/div[1]/div[2]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
+        bEle = document.evaluate("/html/body/div[1]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
+        bEle = document.evaluate("/html/body/div[4]/div/div[1]/div[6]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
+        bEle = document.evaluate("/html/body/div[4]/div/div[1]/div[5]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
+        bEle = document.evaluate("/html/body/div[4]/div/div[1]/div[4]/p", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
+        bEle = document.evaluate("/html/body/div[4]/div/div[3]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
+        bEle = document.evaluate("/html/body/div[4]/div/div[1]/div[5]/div[1]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
+        bEle = document.evaluate("/html/body/div[4]/div/div[3]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
+        bEle = document.evaluate("/html/body/div[4]/div/div[3]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
+        bEle = document.evaluate("/html/body/div[5]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
+        bEle = document.evaluate("/html/body/div[4]/div/div[1]/div[5]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
+        bEle = document.evaluate("/html/body/div[4]/div/div[2]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
+        bEle = document.evaluate("/html/body/div[1]/div[2]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
+        bEle = document.evaluate("/html/body/div[1]/div[2]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
+        bEle = document.evaluate("/html/body/div[1]/div[2]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
+        bEle = document.evaluate("/html/body/div[1]/div[2]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
+        bEle = document.evaluate("/html/body/div[1]/div[2]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
+        bEle = document.evaluate("/html/body/div[1]/div[2]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
+        bEle = document.evaluate("/html/body/div[1]/div[2]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        bEle.singleNodeValue.remove();
     }
-    catch(k){
-        console.log("Script for b.faloo.com ran into an error. Text will show below:");
-        console.log(k.message);
-    }
+    catch(k){}
 })();
