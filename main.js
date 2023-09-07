0274cac1f64bd5c2b4079627e7bede734701eaf8
Gong Hengjie <2682786816@qq.com>
2023-09-08 07:32:05 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name            飞卢小说图片去除
+// @name:en         Faloo Picture Remover
+// @description     用于删除飞卢小说网页端阅读页面的图片
+// @description:en  Used to delete images from the webpage reading page of faloo novel
+// @version         0.0.1
+// @author          Confringo
+// @match           *://b.faloo.com/*_*.html
+// @match           *://wap.faloo.com/*_*.html
+// @license         AGPL-3.0-or-later
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    try{
+        let e = document.evaluate("/html/body/section/div[1]/div[3]/div[1]/a/img", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        e.singleNodeValue.remove();
+    }
+    catch(k){
+        console.log("Script for wap.faloo.com ran into an error. Text will show below:");
+        console.log(k.message);
+    }
+    try{
+        let f = document.evaluate("/html/body/div[6]/div/div[1]/div[6]/a/img", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        f.singleNodeValue.remove();
+    }
+    catch(k){
+        console.log("Script for b.faloo.com ran into an error. Text will show below:");
+        console.log(k.message);
+    }
+})();
